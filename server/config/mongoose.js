4f527795a54b63eb9d5a714c1c2892f64d735c1a
Alan Tsai <alanalan216@gmail.com>
2016-02-09 11:12:23 -0800

    Update mongoose.js

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,20 +1,3 @@
-// var mongoose = require("mongoose");
-//
-// var fs = require("fs"); // for loading model files;
-//
-// var path = require("path"); // for getting model path;
-//
-// // connect to mongoose;
-//
-// mongoose.connect('mongodb://localhost/questionsdb');
-// var models_path = __dirname + '/../models';
-//
-// fs.readdirSync(models_path).forEach(function(file){
-//   if (file.indexOf('.js') >= 0){
-//     require(models_path+'/'+file);
-//   }
-// });
-
 var mongoose = require("mongoose");
 
 var fs = require("fs");
