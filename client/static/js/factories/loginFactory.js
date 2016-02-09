bc396c117048b61905ceed2d2fbb1637797fdc21
Alan Tsai <alanalan216@gmail.com>
2016-02-09 11:14:26 -0800

    Update loginFactory.js

--- client/static/js/factories/loginFactory.js
+++ client/static/js/factories/loginFactory.js
@@ -1,17 +1,3 @@
-// var myApp = angular.module("myApp");
-//
-// myApp.factory('LoginFactory', function($http){
-//   var factory = {};
-//   factory.setUsername = function(username){
-//     localStorage.setItem("username", username);
-//   };
-//
-//   factory.getUsername = function(){
-//     return localStorage.getItem("username");
-//   };
-//   return factory;
-// });
-
 var app = angular.module('app');
 
 app.factory("LoginFactory", function($http){
